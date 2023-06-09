902725cc88449609d697da797483afae43a92614
asha <98428140+jtc21am@users.noreply.github.com>
2023-06-09 12:40:04 -0400

    Update resumeData.ts

--- data/resumeData.ts
+++ data/resumeData.ts
@@ -2,10 +2,9 @@ const resumeData = [
   {
     title: 'Resume',
     description: '',
-    embedSrc: '/static/images/project/ashamaurya_resume.pdf',
+    embedSrc: 'https://onedrive.live.com/view.aspx?resid=B972E0CE7D8636BC!204&ithint=file%2cdocx&authkey=!AB8DGLgD4gbTQrs',
     tag: ['resume'],
   },
 ];
 
 export default resumeData;
-  
